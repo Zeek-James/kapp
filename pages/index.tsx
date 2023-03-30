00291dec0c77eed9ac4157493d5165fa870494e2
Isaac James <chizuruokej@yahoo.com>
2023-03-30 11:22:46 +0100

    Cleans up index Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,8 +10,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <main className={`${styles.main} text-gray`}>
+        <h1 className="text-pink-200">Hello World... Kapp</h1>
       </main>
     </>
   );
