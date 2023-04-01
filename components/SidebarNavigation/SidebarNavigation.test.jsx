bf3d2cf25de396c4f6dd324f704d655237355904
Isaac James <chizuruokej@yahoo.com>
2023-04-01 10:59:41 +0100

    Tests for sidebar navigation and mocks next router

--- components/SidebarNavigation/SidebarNavigation.test.jsx
+++ components/SidebarNavigation/SidebarNavigation.test.jsx
@@ -1,10 +1,42 @@
-import { render, screen } from "@testing-library/react";
-import { SidebarNavigation } from "./";
 import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { useRouter } from "next/router";
+import { SidebarNavigation } from "./";
 
-describe("Home", () => {
-  it("renders a heading", () => {
+// Mock the useRouter hook
+// eslint-disable-next-line no-undef
+jest.mock("next/router", () => ({
+  // eslint-disable-next-line no-undef
+  useRouter: jest.fn(),
+}));
+
+describe("sidebar navigation", () => {
+  it("renders correctly", () => {
+    const mockRouter = {
+      pathname: "/note-list",
+    };
+    useRouter.mockReturnValue(mockRouter);
+
     render(<SidebarNavigation />);
-    expect(screen.getByTestId("container")).toBeInTheDocument();
+    expect(screen.getByTestId("list")).toBeInTheDocument();
+  });
+});
+
+describe("Sidebar component", () => {
+  it("renders list of links with active link highlighted", () => {
+    // Set up mock router object
+    const mockRouter = {
+      pathname: "/note-list",
+    };
+    useRouter.mockReturnValue(mockRouter);
+
+    render(<SidebarNavigation />);
+
+    const menuLink = screen.getAllByRole("menu-item");
+    // Assert that the "About" link is active
+    expect(menuLink[0]).toHaveClass("bg-gray-400");
+
+    // Assert that the other links are not active
+    expect(menuLink[1]).not.toHaveClass("bg-gray-400");
   });
 });
